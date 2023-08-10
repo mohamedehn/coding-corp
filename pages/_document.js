cd7ff6a55950418f31903a5d05225a38e32baf22
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-08-10 13:31:24 +0200

    Modifying _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        <link rel="icon" href="../public/assets/favicon.png" />
-        <link rel="apple-touch-icon" href="../public/assets/favicon.png" />
+        <link rel="icon" href="/assets/favicon.png" />
+        <link rel="apple-touch-icon" href="/assets/favicon.png" />
       </Head>
       <body>
         <Main />
